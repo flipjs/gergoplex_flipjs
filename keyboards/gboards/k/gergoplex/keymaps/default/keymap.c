edb69c9f2476b7c3c3544f407ca1a708063eccf8
Felipe Apostol <flipjs.io@gmail.com>
2020-11-07 13:54:47 -0600

    put mods bindings back in 3rd row and alfred shortcut

--- keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
@@ -32,16 +32,16 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_gergoplex(
-KC_Q,               KC_W,             KC_E,             KC_R,                 KC_T,                  KC_Y,                KC_U,  KC_I,     KC_O,    KC_P,
-KC_A,               KC_S,             KC_D,             KC_F,                 KC_G,                  KC_H,                KC_J,  KC_K,     KC_L,    LT(FUN,KC_SCLN),
-MT(MOD_LSFT,KC_Z),  KC_X,             KC_C,             KC_V,                 MEH_T(KC_B),           MEH_T(KC_N),         KC_M,  KC_COMM,  KC_DOT,  MT(MOD_RSFT,KC_SLSH),
-LT(NUMB,KC_TAB),    LT(SYMB,KC_ESC),  LT(NAVI,KC_SPC),  MT(MOD_LCTL,KC_ENT),  MT(MOD_LGUI,KC_BSPC),  MT(MOD_LALT,KC_DEL)
+KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,                  KC_Y,                KC_U,               KC_I,                  KC_O,                 KC_P,
+KC_A,               KC_S,               KC_D,               KC_F,               KC_G,                  KC_H,                KC_J,               KC_K,                  KC_L,                 LT(FUN,KC_SCLN),
+MT(MOD_LSFT,KC_Z),  MT(MOD_LALT,KC_X),  MT(MOD_LCTL,KC_C),  MT(MOD_LGUI,KC_V),  MEH_T(KC_B),           MEH_T(KC_N),         MT(MOD_LGUI,KC_M),  MT(MOD_LCTL,KC_COMM),  MT(MOD_LALT,KC_DOT),  MT(MOD_RSFT,KC_SLSH),
+LT(NUMB,KC_TAB),    LT(SYMB,KC_ESC),    LT(NAVI,KC_SPC),    LT(NAVI,KC_ENT),    MT(MOD_LCTL,KC_BSPC),  MT(MOD_LGUI,KC_DEL)
 ),
 [NAVI] = LAYOUT_gergoplex(
-KC_NO,  KC_NO,    KC_UP,    KC_NO,     KC_BTN3,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_VOLU,
-KC_NO,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_BTN4,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_MUTE,
-KC_NO,  KC_NO,    KC_NO,    KC_NO,     KC_BTN5,  KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,  KC_NO,
-KC_NO,  KC_NO,    KC_NO,    KC_ENT,    KC_BTN1,  KC_BTN2
+KC_NO,  KC_NO,    KC_UP,    KC_NO,         KC_BTN3,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_VOLU,
+KC_NO,  KC_LEFT,  KC_DOWN,  KC_RIGHT,      KC_BTN4,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_MUTE,
+KC_NO,  KC_NO,    KC_NO,    KC_NO,         KC_BTN5,  KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,  KC_NO,
+KC_NO,  KC_NO,    KC_SPC,   LGUI(KC_SPC),  KC_BTN1,  KC_BTN2
 ),
 [SYMB] = LAYOUT_gergoplex(
 KC_NO,  KC_NO,    KC_UP,    KC_NO,     KC_NO,    KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
