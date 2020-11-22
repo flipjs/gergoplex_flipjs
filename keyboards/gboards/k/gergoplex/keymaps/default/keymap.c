f27be4ef37e94f56a2450d845535deea84b49254
Felipe Apostol <flipjs.io@gmail.com>
2020-11-21 21:16:03 -0600

    move numpad to the lefthand side of the board

--- keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
@@ -11,9 +11,8 @@
 
 #define BASE 0 // default layer
 #define NAVI 1 // nav layer
-#define SYMB 2 // symbol layer
-#define NUMB 3 // number layer
-#define FUN  4 // function layer
+#define NUMB 2 // number layer
+#define FUN  3 // function layer
 
 /* Combomap
  *
@@ -32,33 +31,27 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_gergoplex(
-KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,                  KC_Y,                KC_U,               KC_I,                  KC_O,                 KC_P,
-KC_A,               KC_S,               KC_D,               KC_F,               KC_G,                  KC_H,                KC_J,               KC_K,                  KC_L,                 LT(FUN,KC_SCLN),
-MT(MOD_LSFT,KC_Z),  MT(MOD_LALT,KC_X),  MT(MOD_LCTL,KC_C),  MT(MOD_LGUI,KC_V),  MEH_T(KC_B),           MEH_T(KC_N),         MT(MOD_LGUI,KC_M),  MT(MOD_LCTL,KC_COMM),  MT(MOD_LALT,KC_DOT),  MT(MOD_RSFT,KC_SLSH),
-KC_LGUI,            MO(NUMB),           LT(NAVI,KC_SPC),    LT(NAVI,KC_ENT),    MT(MOD_LCTL,KC_BSPC),  MT(MOD_LALT,KC_TAB)
+KC_Q,               KC_W,               KC_E,               KC_R,               KC_T,              KC_Y,           KC_U,               KC_I,                  KC_O,                 KC_P,
+KC_A,               KC_S,               KC_D,               KC_F,               KC_G,              KC_H,           KC_J,               KC_K,                  KC_L,                 KC_SCLN,
+MT(MOD_LSFT,KC_Z),  MT(MOD_LALT,KC_X),  MT(MOD_LCTL,KC_C),  MT(MOD_LGUI,KC_V),  MEH_T(KC_B),       MEH_T(KC_N),    MT(MOD_LGUI,KC_M),  MT(MOD_LCTL,KC_COMM),  MT(MOD_LALT,KC_DOT),  MT(MOD_RSFT,KC_SLSH),
+KC_LGUI,            KC_LCTL,            LT(NAVI,KC_SPC),    LT(NAVI,KC_ENT),    LT(NUMB,KC_BSPC),  LT(FUN,KC_TAB)
 ),
 [NAVI] = LAYOUT_gergoplex(
-KC_VOLU,  KC_NO,    KC_UP,    KC_NO,         KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO,    KC_NO,
-KC_MUTE,  KC_LEFT,  KC_DOWN,  KC_RIGHT,      KC_NO,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_NO,
-KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,       KC_NO,  KC_NO,    KC_NO,    KC_NO,  KC_NO,    KC_NO,
-KC_NO,    KC_NO,    KC_SPC,   LGUI(KC_SPC),  KC_NO,  KC_NO
-),
-[SYMB] = LAYOUT_gergoplex(
-KC_ESC,      KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
-LCTL(KC_A),  KC_NO,    KC_NO,    KC_NO,    KC_TAB,   KC_UNDS,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_DQUO,
-KC_LSFT,     KC_LALT,  KC_LCTL,  KC_LGUI,  KC_SPC,   KC_TILD,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,
-KC_NO,       KC_NO,    KC_NO,    KC_ENT,   KC_BSPC,  KC_NO
+KC_VOLU,  KC_NO,    KC_UP,    KC_NO,         KC_ESC,   KC_NO,    KC_NO,    KC_NO,  KC_NO,    KC_DEL,
+KC_MUTE,  KC_LEFT,  KC_DOWN,  KC_RIGHT,      KC_TAB,   KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_BSPC,
+KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,       KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,    KC_NO,
+KC_NO,    KC_NO,    KC_SPC,   LGUI(KC_SPC),  KC_BSPC,  KC_DEL
 ),
 [NUMB] = LAYOUT_gergoplex(
-KC_ESC,      KC_NO,    KC_LPRN,  KC_RPRN,  KC_NO,    KC_EQL,   KC_1,  KC_2,  KC_3,  KC_BSLS,
-LCTL(KC_A),  KC_NO,    KC_LCBR,  KC_RCBR,  KC_TAB,   KC_MINS,  KC_4,  KC_5,  KC_6,  KC_QUOT,
-KC_LSFT,     KC_LALT,  KC_LBRC,  KC_RBRC,  KC_SPC,   KC_GRV,   KC_7,  KC_8,  KC_9,  KC_0,
-KC_NO,       KC_NO,    KC_SPC,   KC_ENT,   KC_BSPC,  KC_DOT
+KC_GRV,   KC_1,     KC_2,    KC_3,    KC_EQL,   KC_NO,  KC_LPRN,  KC_RPRN,  KC_NO,  KC_DEL,
+KC_QUOT,  KC_4,     KC_5,    KC_6,    KC_MINS,  KC_NO,  KC_LCBR,  KC_RCBR,  KC_NO,  KC_BSPC,
+KC_BSLS,  KC_7,     KC_8,    KC_9,    KC_0,     KC_NO,  KC_LBRC,  KC_RBRC,  KC_NO,  KC_LSFT,
+KC_LGUI,  KC_LCTL,  KC_SPC,  KC_ENT,  KC_NO,    KC_NO
 ),
 [FUN] = LAYOUT_gergoplex(
-KC_PSCR,  KC_F1,   KC_F2,  KC_F3,  KC_F10,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-KC_SLCK,  KC_F4,   KC_F5,  KC_F6,  KC_F11,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-KC_PAUS,  KC_F7,   KC_F8,  KC_F9,  KC_F12,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
-KC_CAPS,  KC_INS,  KC_NO,  KC_NO,  RESET,   KC_NO
+KC_PSCR,  KC_F1,    KC_F2,   KC_F3,   KC_F12,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+KC_SLCK,  KC_F4,    KC_F5,   KC_F6,   KC_F11,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+KC_PAUS,  KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+KC_INS,   KC_CAPS,  KC_SPC,  KC_ENT,  RESET,   KC_NO
 )
 };
