4bef61b612967efd68b68e728ff75dc8b99fb86b
Felipe Apostol <fapostol@gogoair.com>
2020-11-22 20:49:04 -0600

    add esc binding on lhs for single-hand use

--- keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
@@ -44,13 +44,13 @@ KC_LSFT,        KC_LALT,       KC_LCTL,  KC_LGUI,       KC_MEH,   KC_MEH,   KC_L
 LSFT(KC_COMM),  LSFT(KC_DOT),  KC_SPC,   LGUI(KC_SPC),  KC_LCTL,  KC_LGUI
 ),
 [SYMB] = LAYOUT_gergoplex(
-KC_NO,       KC_NO,    KC_NO,    KC_NO,    LCTL(KC_P),  KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
+KC_ESC,      KC_NO,    KC_NO,    KC_NO,    LCTL(KC_P),  KC_PLUS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_PIPE,
 LCTL(KC_A),  KC_NO,    KC_NO,    KC_NO,    KC_TAB,      KC_UNDS,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_DQUO,
 KC_LSFT,     KC_LALT,  KC_LCTL,  KC_LGUI,  LCTL(KC_N),  KC_TILD,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,
 KC_NO,       KC_NO,    KC_SPC,   KC_ENT,   KC_BSPC,     KC_DEL
 ),
 [NUMB] = LAYOUT_gergoplex(
-KC_NO,       KC_NO,    KC_LPRN,  KC_RPRN,  LCTL(KC_P),  KC_EQL,   KC_1,  KC_2,  KC_3,  KC_BSLS,
+KC_ESC,      KC_NO,    KC_LPRN,  KC_RPRN,  LCTL(KC_P),  KC_EQL,   KC_1,  KC_2,  KC_3,  KC_BSLS,
 LCTL(KC_A),  KC_NO,    KC_LCBR,  KC_RCBR,  KC_TAB,      KC_MINS,  KC_4,  KC_5,  KC_6,  KC_QUOT,
 KC_LSFT,     KC_LALT,  KC_LBRC,  KC_RBRC,  LCTL(KC_N),  KC_GRV,   KC_7,  KC_8,  KC_9,  KC_0,
 KC_NO,       KC_NO,    KC_SPC,   KC_ESC,   KC_LCTL,     KC_LGUI
