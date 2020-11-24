3aef761f878bf0e208c5466aab4670a4cddba538
Felipe Apostol <fapostol@gogoair.com>
2020-11-24 17:49:03 -0600

    delete esc thumb binding; add esc binding top left

--- keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ MT(MOD_LSFT,KC_Z),  MT(MOD_LALT,KC_X),  MT(MOD_LCTL,KC_C),  MT(MOD_LGUI,KC_V),  
 MO(SYMB),           MO(NUMB),           LT(NAVI,KC_SPC),    LT(NAVI,KC_ENT),    MT(MOD_LCTL,KC_BSPC),  KC_LGUI
 ),
 [NAVI] = LAYOUT_gergoplex(
-KC_NO,          KC_NO,         KC_UP,    KC_NO,         KC_NO,    KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_VOLU,
+KC_ESC,         KC_NO,         KC_UP,    KC_NO,         KC_NO,    KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_VOLU,
 KC_NO,          KC_LEFT,       KC_DOWN,  KC_RIGHT,      KC_NO,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_MUTE,
 KC_LSFT,        KC_LALT,       KC_LCTL,  KC_LGUI,       KC_MEH,   KC_MEH,   KC_LGUI,  KC_LCTL,  KC_LALT,  KC_LSFT,
 LSFT(KC_COMM),  LSFT(KC_DOT),  KC_SPC,   LGUI(KC_SPC),  KC_LCTL,  KC_LGUI
@@ -53,7 +53,7 @@ KC_NO,       KC_NO,    KC_SPC,   KC_ENT,   KC_BSPC,     KC_DEL
 KC_ESC,      KC_NO,    KC_LPRN,  KC_RPRN,  LCTL(KC_P),  KC_EQL,   KC_1,  KC_2,  KC_3,  KC_BSLS,
 LCTL(KC_A),  KC_NO,    KC_LCBR,  KC_RCBR,  KC_TAB,      KC_MINS,  KC_4,  KC_5,  KC_6,  KC_QUOT,
 KC_LSFT,     KC_LALT,  KC_LBRC,  KC_RBRC,  LCTL(KC_N),  KC_GRV,   KC_7,  KC_8,  KC_9,  KC_0,
-KC_NO,       KC_NO,    KC_SPC,   KC_ESC,   KC_LCTL,     KC_LGUI
+KC_NO,       KC_NO,    KC_SPC,   KC_ENT,   KC_LCTL,     KC_LGUI
 ),
 [FUN] = LAYOUT_gergoplex(
 KC_PSCR,  KC_F1,    KC_F2,  KC_F3,  KC_F10,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
